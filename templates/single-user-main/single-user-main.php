eb2534b78a4042ad85dfefaa5f04dbad6b820db7
Kirill <kirillian@gmail.com>
2022-08-01 14:15:51 +0300

    Starting single user template

--- templates/single-user-main/single-user-main.php
+++ templates/single-user-main/single-user-main.php
@@ -6,4 +6,36 @@
 // Security check - exit if accessed directly
 defined('ABSPATH') || exit;
 
+$title_level = 'h3'; // default TODO: in settings
+
 ?>
+
+<div class="kw-userdisplay-single-user">
+
+    <div class="kw-user-title">
+
+        <<?php echo $title_level; ?>>
+
+            <?php echo $user_title; ?>
+
+        </<?php echo $title_level; ?>>
+
+    </div>
+
+    <div class="kw-user-image">
+
+        <?php echo $user_image; ?>
+
+    </div>
+
+    <div class="kw-user-name">
+
+        <?php echo $user_name; ?>
+        
+    </div>
+
+    <div class="kw-about-text">
+
+    </div>
+
+</div>
